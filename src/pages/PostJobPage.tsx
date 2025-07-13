0ad9e4244f0204eb36b975861ee4a8d6e96b2e8b
Sleepymates <frank@sunnychats.com>
2025-07-13 19:57:05 +0200

    Fix Input import in PostJobPage

--- src/pages/PostJobPage.tsx
+++ src/pages/PostJobPage.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Card } from '../components/ui/Card';
-import { Input } from '../components/ui/Input';
-import { TextArea } from '../components/ui/TextArea';
+import Card from '../components/ui/Card';
+import Input from '../components/ui/Input';
+import TextArea from '../components/ui/TextArea';
 import Button from '../components/ui/button';
 import BubbleInput from '../components/ui/BubbleInput';
 import { generateJobId } from '../utils/generateId';
